d6c486360d89616b26a7d3175bfd566caad9cdcf
hzy1kk <lucaslohan@colegiopaulodetarso.com.br>
2025-05-14 16:19:01 +0000

    .

---
+++ ex3.py
@@ -0,0 +1,6 @@
+
+N = 100
+soma = 0
+for i in range(1, N + 1):
+    soma += i
+print(soma)
---
+++ ex4.py
@@ -0,0 +1,7 @@
+i = 1
+soma_pares = 0
+while i <= 50:
+    if i % 2 == 0:
+        soma_pares += i
+    i += 1
+print(soma_pares)
